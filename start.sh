34d8e2ac36d570854963e7bf9a71b957b6dee184
Dimokus <Dimokus@mail.ru>
2022-08-17 18:17:45 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -36,17 +36,32 @@ USERPASS=$(goxkcdpwgen -n 2 -c -d "-")
 sleep 10
 ./vpnserver start
 sleep 10
-./vpncmd /SERVER 127.0.0.1 /CMD SstpEnable no
-./vpncmd /SERVER 127.0.0.1 /CMD SyslogDisable
-./vpncmd /SERVER 127.0.0.1 /CMD ServerCipherSet ECDHE-RSA-AES128-GCM-SHA256
-./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SecureNatEnable
-./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD LogDisable packet
-./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserCreate $USER /GROUP:none /REALNAME:none /NOTE:none
-{ echo $USERPASS; echo $USERPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserPasswordSet $USER
- ./vpncmd /SERVER 127.0.0.1 /CMD IPsecEnable /L2TP:yes /L2TPRAW:no /ETHERIP:no /PSK:$PSKPASS /DEFAULTHUB:DEFAULT
-{ echo $ADMINPASS; echo $ADMINPASS; } | ./vpncmd /SERVER 127.0.0.1 /ADMINHUB:DEFAULT /CMD ServerPasswordSet
-{ echo $HUBPASS; echo $HUBPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SetHubPassword
-./vpncmd /SERVER /PASSWORD:$ADMINPASS localhost /CMD OpenVpnMakeConfig openvpn
+ADMINPASS=$(goxkcdpwgen -n 2 -c -d "-")
+  HUBPASS=$(goxkcdpwgen -n 2 -c -d "-")
+  PSKPASS=$(openssl rand -hex 4)
+  
+  ./vpncmd /SERVER 127.0.0.1 /CMD SstpEnable no
+  ./vpncmd /SERVER 127.0.0.1 /CMD SyslogDisable
+  ./vpncmd /SERVER 127.0.0.1 /CMD ServerCipherSet ECDHE-RSA-AES128-GCM-SHA256
+  
+  ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SecureNatEnable
+  ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD LogDisable packet
+  
+  ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserCreate $USER /GROUP:none /REALNAME:none /NOTE:none
+  { echo $USERPASS; echo $USERPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserPasswordSet $USER
+  
+  # To enable Anonymous mode (any user password will work):
+  #./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserAnonymousSet $USER
+  
+  # specify the default hub so you don't need to specify "user@<hub name>"
+  ./vpncmd /SERVER 127.0.0.1 /CMD IPsecEnable /L2TP:yes /L2TPRAW:no /ETHERIP:no /PSK:$PSKPASS /DEFAULTHUB:DEFAULT
+
+  { echo $ADMINPASS; echo $ADMINPASS; } | ./vpncmd /SERVER 127.0.0.1 /ADMINHUB:DEFAULT /CMD ServerPasswordSet
+  { echo $HUBPASS; echo $HUBPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SetHubPassword
+
+  # OpenVPN config
+  ./vpncmd /SERVER /PASSWORD:$ADMINPASS localhost /CMD OpenVpnMakeConfig openvpn
+  unzip openvpn.zip '*_openvpn_remote_access_l3.ovpn'
  unzip openvpn.zip '*_openvpn_remote_access_l3.ovpn'
  file=`ls | grep .ovpn`
  cp $file /var/www/html
