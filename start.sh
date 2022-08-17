067be3d830ab5de0369fdac11ed14ad4b9e4603e
Dimokus <Dimokus@mail.ru>
2022-08-17 12:45:06 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,6 +34,7 @@ USER=$(goxkcdpwgen -n 1)
 USERPASS=$(goxkcdpwgen -n 2 -c -d "-")
 sleep 10
 ./vpnserver start
+sleep 10
 ./vpncmd /SERVER 127.0.0.1 /CMD SstpEnable no
 ./vpncmd /SERVER 127.0.0.1 /CMD SyslogDisable
 ./vpncmd /SERVER 127.0.0.1 /CMD ServerCipherSet ECDHE-RSA-AES128-GCM-SHA256
@@ -41,3 +42,8 @@ sleep 10
 ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD LogDisable packet
 ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserCreate $USER /GROUP:none /REALNAME:none /NOTE:none
 { echo $USERPASS; echo $USERPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserPasswordSet $USER
+./vpncmd /SERVER 127.0.0.1 /CMD IPsecEnable /L2TP:yes /L2TPRAW:no /ETHERIP:no /PSK:$PSKPASS /DEFAULTHUB:DEFAULT
+{ echo $ADMINPASS; echo $ADMINPASS; } | ./vpncmd /SERVER 127.0.0.1 /ADMINHUB:DEFAULT /CMD ServerPasswordSet
+{ echo $HUBPASS; echo $HUBPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SetHubPassword
+./vpncmd /SERVER /PASSWORD:$ADMINPASS localhost /CMD OpenVpnMakeConfig openvpn
+ unzip openvpn.zip '*_openvpn_remote_access_l3.ovpn'
