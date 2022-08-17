d2472e72bb9f62c6a8019c0a744adb8f1560a82a
Dimokus <Dimokus@mail.ru>
2022-08-17 12:08:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,15 +10,13 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 service ssh restart
 sleep 5
-sudo apt-get install -y nano runit
-runsvdir -P /etc/service &
 source $HOME/.bashrc
 wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.39-9772-beta/softether-vpnserver-v4.39-9772-beta-2022.04.26-linux-x64-64bit.tar.gz
 tar xvzf softether-vpnserver-v4.39-9772-beta-2022.04.26-linux-x64-64bit.tar.gz
 cd vpnserver
 make
 
-CONFIG=/root/vpnserver/vpn_server.config
+CONFIG=/vpnserver/vpn_server.config
 line=$(grep -n  'bool Disabled true' $CONFIG | awk -F ":" '{print $1}')
 sed -i $line's/true/false/' $CONFIG
 
@@ -32,14 +30,12 @@ PSKPASS=$(openssl rand -hex 4)
 USER=$(goxkcdpwgen -n 1)
 USERPASS=$(goxkcdpwgen -n 2 -c -d "-")
 cp vpnserver /usr/bin
+cp vpncmd /usr/bin
 vpnserver start
-./vpncmd /SERVER 127.0.0.1 /CMD SstpEnable no
-  ./vpncmd /SERVER 127.0.0.1 /CMD SyslogDisable
-  ./vpncmd /SERVER 127.0.0.1 /CMD ServerCipherSet ECDHE-RSA-AES128-GCM-SHA256
-  
-  ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SecureNatEnable
-  ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD LogDisable packet
-  
-  ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserCreate $USER /GROUP:none /REALNAME:none /NOTE:none
-  { echo $USERPASS; echo $USERPASS; } | ./vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserPasswordSet $USER
-
+vpncmd /SERVER 127.0.0.1 /CMD SstpEnable no
+vpncmd /SERVER 127.0.0.1 /CMD SyslogDisable
+vpncmd /SERVER 127.0.0.1 /CMD ServerCipherSet ECDHE-RSA-AES128-GCM-SHA256
+vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD SecureNatEnable
+vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD LogDisable packet
+vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserCreate $USER /GROUP:none /REALNAME:none /NOTE:none
+{ echo $USERPASS; echo $USERPASS; } | vpncmd /SERVER 127.0.0.1 /HUB:DEFAULT /CMD UserPasswordSet $USER
