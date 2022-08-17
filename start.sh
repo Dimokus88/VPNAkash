4564f6d629448225cdce05ff1a0746f8ccc99992
Dimokus <Dimokus@mail.ru>
2022-08-17 12:14:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,10 @@ wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.39
 tar xvzf softether-vpnserver-v4.39-9772-beta-2022.04.26-linux-x64-64bit.tar.gz
 cd vpnserver
 make
-
+cp vpnserver /usr/bin
+cp vpncmd /usr/bin
+vpnserver start
+vpnserver stop
 CONFIG=/vpnserver/vpn_server.config
 line=$(grep -A 19 -n DDnsClient $CONFIG | grep -m1 -B19 "}" | grep "bool Disabled" | awk -F "-" '{print $1}')
 sed -i $line's/false/true/' $CONFIG
@@ -26,8 +29,6 @@ PSKPASS=$(openssl rand -hex 4)
 
 USER=$(goxkcdpwgen -n 1)
 USERPASS=$(goxkcdpwgen -n 2 -c -d "-")
-cp vpnserver /usr/bin
-cp vpncmd /usr/bin
 vpnserver start
 vpncmd /SERVER 127.0.0.1 /CMD SstpEnable no
 vpncmd /SERVER 127.0.0.1 /CMD SyslogDisable
