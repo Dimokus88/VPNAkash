d585848e335dd45447d9308d97bb3d35ce30bed0
Dimokus <Dimokus@mail.ru>
2022-08-18 13:36:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,7 @@ TZ=Europe/Kiev
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update
 apt-get upgrade -y
-apt-get install -y sudo nano wget tar zip unzip ssh nginx xz-utils make gcc goxkcdpwgen
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-(echo ${my_root_password}; echo ${my_root_password}) | passwd root
-service ssh restart
+apt-get install -y sudo wget tar nginx xz-utils make gcc goxkcdpwgen
 sleep 5
 source $HOME/.bashrc
 wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.39-9772-beta/softether-vpnserver-v4.39-9772-beta-2022.04.26-linux-x64-64bit.tar.gz
@@ -25,7 +22,7 @@ sleep 10
 CONFIG=/vpnserver/vpn_server.config
 line=$(grep -A 19 -n DDnsClient $CONFIG | grep -m1 -B19 "}" | grep "bool Disabled" | awk -F "-" '{print $1}')
 sed -i $line's/false/true/' $CONFIG
-ine=$(grep -n DisableJsonRpcWebApi $CONFIG |awk -F ":" '{print $1}')
+line=$(grep -n DisableJsonRpcWebApi $CONFIG |awk -F ":" '{print $1}')
 sed -i $line's/false/true/' $CONFIG
 ADMINPASS=$(goxkcdpwgen -n 2 -c -d "-")
 HUBPASS=$(goxkcdpwgen -n 2 -c -d "-")
